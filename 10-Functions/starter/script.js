192956a30f43c58c407cda0b1ee15da953fcbebb
jking026 <jamesjking026@gmail.com>
2021-02-25 11:52:30 +0900

    😭 CC#1-complete with help

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -229,3 +229,60 @@ const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
 */
+/*
+////////////////////CODING CHALLENGE #1 S10////////////////////////
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  //this is called a METHOD not a function
+  registerNewAnswer() {
+    // Get the answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`"Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+
+// 1.
+Create a method called 'registerNewAnswer' on the 'poll' object.
+ a) Display a prompt window for the user to input the number of the
+selected option.
+
+
+// 2. Call his method whenevSer the user clicks the answer poll
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+//displayResult => type string .toLowerCase()
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+/*Test data for bonus:
+§ Data 1: [5, 2, 3]
+§ Data 2: [1, 5, 3, 9, 6, 1]
+*/
+
+///Redo necessary----CC#1--- finished with help accept for button function
