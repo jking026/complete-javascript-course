e4dfc9f6b672e60d786d4ad0b6f3f12030f1d3eb
jking026 <jamesjking026@gmail.com>
2021-02-25 13:01:25 +0900

    🏁L:  MORE CLOSURES Ex.

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -333,3 +333,53 @@ booker();
 
 console.dir(booker);
 */
+/*
+// LESSON: MORE CLOSURE EXAMPLES//
+
+// Don't need to return a function from another function to use closures
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+// Re-assigning f  function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+// closures take priority in the scope chain
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+// setTimeout function executes the function in miliseconds
+// setTimeout(function () {
+//   console.log(' 🖖🖖🖖🖖🖖🖖🖖🖖🖖');
+// }, 3000);
+*/
