a013fb62d8918296362c6bfb9acd1ef9076948c5
jking026 <jamesjking026@gmail.com>
2021-02-25 16:24:06 +0900

    🏁L: FOREACH INTRO

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -115,5 +115,41 @@ console.log(letters.join(' + '));
 console.log(letters.join(' ? '));
 console.log(letters.join(' 🍸☺ '));
 */
-
+/*
 // LESSON: LOOPING ARRAYS: FOREACH
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// forEach function is a HIGHER ORDER FUCTION (we don't call this function)
+
+// for(const movement of movements)
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log('-----------FOREACH-----------------');
+// ****important*** Order of parameters matter
+// 1st. Current element
+// 2nd. Current index
+// 3rd. Entire array we are looping over
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(450)
+// 2. function(400)
+// ...
+
+// When to use for each/ for of
+//Cannot breakout of a for each loop, because it will loop forever
+*/
+
+// LESSON: forEACH WITH MAPS AND SETS
