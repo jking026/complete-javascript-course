73b7649d58548a1474b042ab3de8aca3e3742181
jking026 <jamesjking026@gmail.com>
2021-03-22 14:03:35 +0900

    🏁L: EVENT PROPAGATION IN PRACTICE

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -175,3 +175,34 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 10000);
 //   alert('addEventListener: Great! You are reading the heading :D');
 // };
 */
+/*
+// LESSON: EVENT PROPAGATION IN PRACTICE
+
+// rgb(255,255, 255)
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+*/
+
+// LESSON: EVENT DELEGATION - IMPLEMENTING PAGE NAVIGATION
