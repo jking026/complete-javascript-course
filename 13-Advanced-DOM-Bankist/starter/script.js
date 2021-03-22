f875d842fc8a53551eb7c7067fef459f01049802
jking026 <jamesjking026@gmail.com>
2021-03-22 22:08:32 +0900

    🏁 L: LIFECYCLE DOM EVENTS

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -510,3 +510,23 @@ console.log(h1.parentElement.children);
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
 */
+
+// LESSON: DOM LIFESTYLE
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree build!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// Don't abuse this!
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault(); // some browsers not Chrome
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
+// LESSON: EFFICIENT SCRIPT LOADING: DEFER AND ASYNC
+/* CHECK GOODNOTES*/
