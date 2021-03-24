76d2c26ec49b9a49d1ac66c5f332f044b899fc4e
jking026 <jamesjking026@gmail.com>
2021-03-24 20:16:56 +0900

    🏁L: ES6 CLASSES

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -112,3 +112,45 @@ mercedes.accelerate();
 */
 
 /////////////////////////////////CC#1 COMPLETE///////////////////
+/*
+// LESSON: ES6 CLASSES
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  //  Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT 'hoisted'
+
+// 2. Classes are first-class citizens (pass them into functions and return them from functions)
+
+//3. Classes are executed in strict mode
+*/
+
+// LESSON: SITTERS AND GETTERS
